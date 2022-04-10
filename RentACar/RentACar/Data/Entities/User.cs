fdf3e6b3231dfd9992b9b3e2b13a6dc9d17bfdd2
miller ossa samboni <miller.ossa@sofka.com.co>
2022-04-10 17:24:24 -0500

    configuracion inicial program, datacontext, clase reserve

---
+++ RentACar/RentACar/Data/Entities/User.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RentACar.Data.Entities
+{
+    public class User: IdentityUser
+    {
+    }
+}
--- RentACar/RentACar/Program.cs
+++ RentACar/RentACar/Program.cs
@@ -1,15 +1,17 @@
+using RentACar.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<DataContext>(o =>  {
+    o.UseSqlServer();
+});
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
