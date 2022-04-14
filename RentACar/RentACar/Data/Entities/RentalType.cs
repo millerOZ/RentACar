6563bd641b4a6e06e8032fa94f68b2ca38592ab3
miller ossa samboni <miller.ossa@sofka.com.co>
2022-04-14 10:18:53 -0500

    avance seedDB entities y model

---
+++ RentACar/RentACar/Data/Entities/RentalType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RentACar.Data.Entities
+{
+    public class RentalType
+    {
+       public int Id { get; set; }
+        [Display(Name = "Descripción")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres")]
+        public string Description { get; set; }
+    }
+}
