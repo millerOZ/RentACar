1c0e6ca59a2c0e1447d036cf9699c76c3ebfbe92
Victor Ol <victoroliveros287151@correo.itm.edu.co>
2022-04-19 22:38:30 -0500

    Add BD

--- RentACar/RentACar/Data/SeedDb.cs
+++ RentACar/RentACar/Data/SeedDb.cs
@@ -1,19 +1,85 @@
-﻿using RentACar.Data.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.Data.Entities;
+using RentACar.Helpers;
 
 namespace RentACar.Data
 {
     public class SeedDb
     {
         private readonly DataContext _context;
-        public SeedDb(DataContext context)
+        private readonly IBlobHelper _blobHelper;
+
+
+        public SeedDb(DataContext context, IBlobHelper blobHelper)
         {
             _context = context;
+            _blobHelper = blobHelper;
         }
         public async Task SeedAsync()
         {
             await _context.Database.EnsureCreatedAsync();
             await CheckReservesAsync();
+            await CheckVehiclesAsync();
+            await CheckCategoriesAsync();
         }
+
+        private async Task CheckCategoriesAsync()
+        {
+            if (!_context.Categories.Any())
+            {
+                _context.Categories.Add(new Category { Name = "Sport" });
+                _context.Categories.Add(new Category { Name = "Off-Road" });
+                _context.Categories.Add(new Category { Name = "Urbanos" });
+                _context.Categories.Add(new Category { Name = "Carros" });
+                _context.Categories.Add(new Category { Name = "Motos" });
+
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        private async Task CheckVehiclesAsync()
+        {
+            if (!_context.Vehicles.Any())
+            {
+                await AddVehicleAsync("ZTS-01G", "TVS", "APACHE RT 160 4v - 2023", "Experimenta su tecnología Smart Xconnect y disfruta su ecosistema de conexión," +
+                    " telemetría en ruta, alerta y asistencia en navegación.", new List<string> { "Motos", "Sport", "Urbanos" }, new List<string> { "Apache160.png", "Apache_160.png" });
+
+                await AddVehicleAsync("QRT-91G", "YAMAHA", "MT-09 - 2023", "Transmisión constante de 6 velocidades, Full Inyeccion," +
+                    " 890cc", new List<string> { "Motos", "Sport" }, new List<string> { "mt-09.jfif", "mt-09_2.jfif" });
+
+                await AddVehicleAsync("TGH-903", "MAZDA", "CX-5 - 2023", "Sistema Inteligente de tracción AWD con Función Off-Road, Diseñada para" +
+                    " maximizar tu comodidad. permite selección de diferentes modos de conducción, Sport, Off-Road.", new List<string> { "Carros", "Sport", "Off-Road" }, new List<string> { "Cx-5.jfif", "CX-5_2.jfif" });
+
+            }
+        }
+
+        private async Task AddVehicleAsync(string plaque, string brand, string serie, string remarks, List<string> categories, List<string> images)
+        {
+            Vehicle vehicle = new()
+            {
+                Plaque = plaque,
+                Brand = brand,
+                Serie = serie,
+                Remarks = remarks,
+                VehicleCategories = new List<VehicleCategory>(),
+                ImageVehicles = new List<ImageVehicle>()
+
+            };
+
+            foreach (string category in categories)
+            {
+                vehicle.VehicleCategories.Add(new VehicleCategory { Category = await _context.Categories.FirstOrDefaultAsync(c => c.Name == category) });
+            }
+
+            foreach (string image in images)
+            {
+                Guid imageId = await _blobHelper.UploadBlobAsync($"{Environment.CurrentDirectory}\\wwwroot\\images\\vehicles\\{image}", "vehicles");
+                vehicle.ImageVehicles.Add(new ImageVehicle { ImageId = imageId });
+            }
+
+            _context.Vehicles.Add(vehicle);
+        }
+
         private async Task CheckReservesAsync()
         {
             if (!_context.Reserves.Any())
@@ -53,6 +119,8 @@ namespace RentACar.Data
 
                 await _context.SaveChangesAsync();
             }
+
+            
         }
     }
 }
